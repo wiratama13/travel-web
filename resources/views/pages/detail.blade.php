31acc3dd13511015478e25f777bbdc26fa284def
wiratama13 <ashidiqinasrulloh@gmail.com>
2023-08-13 14:25:37 +0700

    git commit img-fluid

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -32,8 +32,8 @@
               </p>
               @if ($item->galleries->count())
               <div class="gallery">
-                <div class="xzoom-container">
-                  <img src="{{Storage::url($item->galleries->first()->image)}}" class="xzoom mb-2" id="xzoom-default" xoriginal="{{Storage::url($item->galleries->first()->image)}}">
+                <div class="xzoom-container w-100">
+                  <img src="{{Storage::url($item->galleries->first()->image)}}" class="xzoom mb-2 img-fluid" id="xzoom-default" xoriginal="{{Storage::url($item->galleries->first()->image)}}">
                 </div>
 
                 <div class="xzoom-thumbs">
